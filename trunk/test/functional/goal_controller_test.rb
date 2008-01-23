dbc538d7d1b6130357afde27e95ccbf2096d5da5
JamesDRandall <JamesDRandall@0c30f4d0-6443-0410-8dab-bb90aacf3a18>
2008-01-23 16:30:40 +0000

    Basic unit tests for the goal controller

--- trunk/test/functional/goal_controller_test.rb
+++ trunk/test/functional/goal_controller_test.rb
@@ -1,8 +1,59 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class GoalControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :users, :goals, :goaltypes
+  
+  def setup
+    @controller = GoalController.new
+    @request = ActionController::TestRequest.new
+    @response =  ActionController::TestResponse.new
+  end
+  
+  def test_index
+    login_as(:quentin)
+    get :index
+    assert_response(:success)
+    assert_not_nil(assigns["activegoals"])
+    assert_not_nil(assigns["completedgoals"])
+    assert_equal(5, assigns["activegoals"].length)
+  end
+  
+  def test_new_success
+    original_row_count = Goal.find(:all).length
+    login_as(:quentin)
+    get :new
+    assert_response(:success)
+    post :new, :goal => { :goaltype_id => 1, :target_date => Date.today, :target_weight => 100 }
+    assert_response(:redirect)
+    assert_equal(original_row_count + 1, Goal.find(:all).length)
+  end
+  
+  def test_new_fail
+    original_row_count = Goal.find(:all).length
+    login_as(:quentin)
+    get :new
+    assert_response(:success)
+    post :new, :goal => { :goaltype_id => 1, :target_date => Date.today }
+    assert_response(:success)
+    assert_not_nil(assigns["goal"])
+    assert_not_nil(assigns["goal"].errors)
+    assert_equal(1, assigns["goal"].errors.length)
+    assert_equal(original_row_count, Goal.find(:all).length)
+  end
+  
+  def test_delete_success
+    original_row_count = Goal.find(:all).length
+    login_as(:quentin)
+    get :delete, :goalid => 1
+    assert_response(:redirect)
+    assert_equal(original_row_count - 1, Goal.find(:all).length)
+  end
+  
+  def test_delete_fail
+    original_row_count = Goal.find(:all).length
+    login_as(:aaron)
+    get :delete, :goalid => 1
+    assert_response(:redirect)
+    assert_equal(original_row_count, Goal.find(:all).length)
   end
 end
